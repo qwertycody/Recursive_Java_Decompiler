25d3e411992852fa7941b4da7c89a139d8e0c380
Cody Garrett <cody@garrett.ms>
2019-07-23 17:56:42 -0400

    Update search.sh

--- search.sh
+++ search.sh
@@ -1,4 +1,4 @@
-DECOMPILED_PATH="/c/pathToMyWar/myWar.war"
+DECOMPILED_PATH="/c/pathToMyWar/myWar"
 STRING_TO_SEARCH="FunText is Super Fun"
 EXTENSION_TO_SEARCH="java"
 grep -r -i --include \*.$EXTENSION_TO_SEARCH "$STRING_TO_SEARCH" "$DECOMPILED_PATH"
